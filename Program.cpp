0d51751c37da35d9e4e4f60ce20a8761dcbdd657
Ali Seifi <seifiali84@gmail.com>
2024-04-18 17:31:23 +0330

    Add Person Class and Split Program File Sections by Comments

--- Program.cpp
+++ Program.cpp
@@ -4,10 +4,43 @@
 
 using namespace std;
 
+/****************************
+ *                           *
+ *          Models           *
+ *                           *
+ *****************************/
 
+class Person
+{
+public:
+    // Attributes :
+    string Name;
+    string Family;
+    int Age;
+    int Height;
+    float Weight;
+    bool Gender;
+    string EyeColor;
+    string SkinColor;
+    string Religion;
+    string Location;
+    string Vehicle;
+    double Money;
+
+    // Methods :
+
+};
+
+
+/****************************
+ *                           *
+ *      Peresentation        *
+ *                           *
+ *****************************/
 
 // Program Start :
-int main(){
+int main()
+{
 
     return 0;
 }
