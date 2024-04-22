94959587c78596b5beeba2c5d4baefab9c8768ad
Ali Seifi <seifiali84@gmail.com>
2024-04-22 11:09:13 +0330

    add Person Class to Project

--- Short Project/Program.cpp
+++ Short Project/Program.cpp
@@ -4,7 +4,29 @@
 
 using namespace std;
 
-int main(){
+class Person
+{
+public:
+    // Attributes :
+
+    int Id;
+    string Name;
+    string Family;
+    int Age;
+    int Height;
+    float Weight;
+    // Female : 1 , Male : 0
+    bool Gender;
+    string EyeColor;
+    string SkinColor;
+    string Religion;
+    string Location;
+    string Vehicle;
+    double Money;
+};
+
+int main()
+{
 
     return 0;
 }
