fb80de0c6b97d9eec4ace1ba13c6090dbc5fa15f
Ali Seifi <seifiali84@gmail.com>
2024-04-22 13:43:39 +0330

    add Constructor method and it's overloadings to Person class

--- Short Project/Program.cpp
+++ Short Project/Program.cpp
@@ -23,13 +23,80 @@ public:
     string Location;
     string Vehicle;
     double Money;
-
+    
+    // DB
     static vector<Person> People;
+
+    // Normal Person Constructor for Method Overloading
+    Person(){
+
+    }
+    // Constructor to Create Object From User Input
+    Person(string name, string family, string age, string height, string weight, string gender, string eyeColor, string skinColor, string religion, string location, string vehicle, string money)
+    {
+        Id = 0;
+        if (name != "")
+        {
+            Name = name;
+        }
+        if (family != "")
+        {
+            Family = family;
+        }
+        if (age != "")
+        {
+            Age = stoi(age);
+        }
+        if (height != "")
+        {
+            Height = stoi(height);
+        }
+        if (weight != "")
+        {
+            Weight = stof(weight);
+        }
+        if (gender != "")
+        {
+            if (gender == "1")
+            {
+                Gender = true;
+            }
+            else
+            {
+                Gender = false;
+            }
+        }
+        if (eyeColor != "")
+        {
+            EyeColor = eyeColor;
+        }
+        if (skinColor != "")
+        {
+            SkinColor = skinColor;
+        }
+        if (religion != "")
+        {
+            Religion = religion;
+        }
+        if (location != "")
+        {
+            Location = location;
+        }
+        if (vehicle != "")
+        {
+            Vehicle = vehicle;
+        }
+        if (money != "")
+        {
+            Money = stod(money);
+        }
+    }
 };
 
 int main()
 {
     Person p1;
+    p1.Id = 1;
     p1.Name = "Ahmad";
     p1.Family = "Mohammadi";
     p1.Age = 20;
@@ -44,6 +111,7 @@ int main()
     p1.Money = 37000;
 
     Person p2;
+    p2.Id = 2;
     p2.Name = "Zahra";
     p2.Family = "Ghasemi";
     p2.Age = 22;
@@ -60,6 +128,11 @@ int main()
     Person::People.push_back(p1);
     Person::People.push_back(p2);
 
-    
+    cout << "Enter Your Filters"
+         << "\n";
+
+    cout << "Enter Name : ";
+    string Name;
+
     return 0;
 }
